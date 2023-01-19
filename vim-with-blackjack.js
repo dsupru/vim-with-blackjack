c68e98f0a45783864c030bc384256c4abc9526d8
Dima S <dsuprunsw@gmail.com>
2023-01-19 09:06:08 -0800

    Add first pass on Vim.. with Blackjack.

---
+++ vim-with-blackjack.js
@@ -0,0 +1,35 @@
+var MODE_IN_INSERT = false;
+
+document.addEventListener("keydown", (event) => {
+	console.log(`Pressed ${event.key}`);
+	if (MODE_IN_INSERT) {
+		document.body.style.border = "5px solid red";
+		if (event.key === "Escape") {
+			MODE_IN_INSERT = false;
+			document.body.style.border = "none";
+		}
+	} else {
+		document.body.style.border = "none";
+		if (event.key === "j") {
+			moveWindowSmooth(event, 150, 0);
+		} else if (event.key === "k") {
+			moveWindowSmooth(event, -150, 0);
+		} else if (event.key === "h") {
+			moveWindowSmooth(event, 0, -150);
+		} else if (event.key === "l") {
+			moveWindowSmooth(event, 0, 150);
+		} else if (event.key === "i") {
+			MODE_IN_INSERT = true;
+			document.body.style.border = "5px solid red";
+		}
+	}
+});
+
+function moveWindowSmooth(event, top, left) {
+	event.preventDefault();
+	window.scrollBy({
+	  top: top,
+	  left: left,
+	  behavior: 'smooth'
+	});
+}
